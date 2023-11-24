41e5009a51c5c8aa2df94143fde53f70e50db91e
Ida <idapy@kth.se>
2023-11-24 13:15:13 +0100

    implement visualization
    there are still bugs in the code and especially the merge sort is very weird during visualization

--- sorting.py
+++ sorting.py
@@ -1,132 +1,152 @@
-def main():
-
-    List = [23, 12, 45, 21, 1, 2, 39]
-
-    Inserted_list = insertion(List)
-    print(Inserted_list)
-
-    Selected_list = selection(List)
-    print(Selected_list)
-
-    Merged_list = mergesort(List)
-    print(Merged_list)
-
-    Bubble_list = bubble_sort(List)
-    print(Bubble_list)
+import tkinter as tk
+import random
+import time
 
 
-def insertion(List):
+def bubble_sort(input_list, visualize=False, canvas=None):
+    n = len(input_list)
 
-    list = List
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if input_list[j] > input_list[j+1]:
+                input_list[j], input_list[j+1] = input_list[j+1], input_list[j]
+                if visualize:
+                    update_canvas(canvas, input_list, j, 'purple', j+1, 'pink')
+                    canvas.update()
+                    time.sleep(0.1)
 
-    i = 1
-
-    while i < len(list):
-
-        number = list[i]
+def insertion_sort(input_list, visualize=False, canvas=None):
+    for i in range(1, len(input_list)):
+        key = input_list[i]
         j = i - 1
+        while j >= 0 and key < input_list[j]:
+            input_list[j + 1] = input_list[j]
+            j -= 1
+        input_list[j + 1] = key
+        if visualize:
+            update_canvas(canvas, input_list, j+1, 'purple', i, 'pink')
+            canvas.update()
+            time.sleep(0.1)
 
-        while j >= 0 and list[j] > number:
+def selection_sort(input_list, visualize=False, canvas=None):
+    for i in range(len(input_list)):
+        min_index = i
+        for j in range(i + 1, len(input_list)):
+            if input_list[j] < input_list[min_index]:
+                min_index = j
+        input_list[i], input_list[min_index] = input_list[min_index], input_list[i]
+        if visualize:
+            update_canvas(canvas, input_list, i, 'purple', min_index, 'pink')
+            canvas.update()
+            time.sleep(0.1)
 
-            list[j + 1] = list[j]
-            j-=1
+def merge_sort(input_list, visualize=False, canvas=None):
+    if len(input_list) > 1:
+        mid = len(input_list) // 2
+        left_half = input_list[:mid]
+        right_half = input_list[mid:]
 
-        list[j + 1] = number
-        i = i + 1
+        merge_sort(left_half, visualize, canvas)
+        merge_sort(right_half, visualize, canvas)
 
-    return list
+        i = j = k = 0
 
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                input_list[k] = left_half[i]
+                i += 1
+            else:
+                input_list[k] = right_half[j]
+                j += 1
+            k += 1
 
-def selection(List):
+        while i < len(left_half):
+            input_list[k] = left_half[i]
+            i += 1
+            k += 1
 
-    list = List
-
-    i = 0
-
-    while i < len(list) - 1:
-
-        minIndex = i
-        j = i + 1
-
-        while j < len(list):
-
-            if list[j] < list[minIndex]:
-
-                minIndex = j
-
+        while j < len(right_half):
+            input_list[k] = right_half[j]
             j += 1
+            k += 1
 
-        if minIndex != i:
+        if visualize:
+            update_canvas(canvas, input_list, i+j, 'purple', i, 'pink')
+            canvas.update()
+            time.sleep(0.1)
 
-            swap = list[i]
-            list[i] = list[minIndex]
-            list[minIndex] = swap
+def update_canvas(canvas, heights, first, color_first, second, color_second):
+    canvas.delete("all")
+    bar_width = canvas_width / len(heights)
+    for i, height in enumerate(heights):
+        x1, y1 = i * bar_width, canvas_height - height * scale_factor
+        x2, y2 = x1 + bar_width, canvas_height
 
-        i += 1
-    return list
+        if i == first:
 
+            fill_color = color_first
 
-def mergesort(List):
+        elif i == second:
 
-    list = List
-
-    if len(list) == 1:
-
-        return list
-
-    left = list[:len(list)//2]
-    right = list[len(list)//2:]
-
-
-
-    left = mergesort(left)
-    right = mergesort(right)
-    return merge(left, right)
-
-
-def merge(left_list, right_list):
-
-    new_list = []
-
-    while len(left_list) > 0 and len(right_list) > 0:
-
-        if left_list[0] > right_list[0]:
-
-            new_list.append(right_list[0])
-            right_list = right_list[1:]
-
+            fill_color = color_second
         else:
 
-            new_list.append((left_list[0]))
-            left_list = left_list[1: ]
+            fill_color = 'lightblue'
 
-    while len(left_list) > 0:
+        canvas.create_rectangle(x1, y1, x2, y2, fill=fill_color)
 
-        new_list.append(left_list[0])
-        left_list = left_list[1:]
+def start_visualization(input_list):
+    algorithm = algorithm_var.get()
+    sorting_algorithms[algorithm](input_list.copy(), visualize=True, canvas=canvas)
 
-    while len(right_list) > 0:
+def reset(input_list):
 
-        new_list.append(right_list[0])
-        right_list = right_list[1:]
+    canvas.delete("all")
+    update_canvas(canvas, input_list, None, 'purple', None, 'pink')
+    canvas.update_idletasks()
 
-    return new_list
+def initialize_window(input_list):
+
+    canvas.delete("all")
+    update_canvas(canvas, input_list, None, 'purple', None, 'pink')
+    canvas.update_idletasks()
 
 
-def bubble_sort(List):
+root = tk.Tk()
+root.title("Sorting Visualizer")
 
-    list = List
-    n = len(list)
+screen_height = root.winfo_screenheight()
+canvas_height = int(screen_height * 0.8)
 
-    # Traverse through all elements in the list
-    for i in range(n):
-        # Last i elements are already in place, so we don't need to check them
-        for j in range(0, n-i-1):
-            # Swap if the element found is greater than the next element
-            if list[j] > list[j+1]:
-                list[j], list[j+1] = list[j+1], list[j]
+scale_factor = canvas_height / 20
 
-    return list
+button_frame = tk.Frame(root)
+button_frame.pack(pady=10)
 
-if __name__ == "__main__":
-    main()
+canvas_width = root.winfo_screenwidth() - 20
+canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg="white")
+canvas.pack()
+
+algorithm_var = tk.StringVar(root)
+algorithm_var.set("Bubble Sort")
+algorithm_menu = tk.OptionMenu(button_frame, algorithm_var, "Bubble Sort", "Insertion Sort", "Selection Sort", "Merge Sort")
+algorithm_menu.pack(side=tk.LEFT, padx=5)
+
+start_button = tk.Button(button_frame, text="Start Visualization", command=lambda: start_visualization(initial_list))
+start_button.pack(side=tk.LEFT, padx=5)
+
+reset_button = tk.Button(button_frame, text="Reset", command=lambda: reset(initial_list))
+reset_button.pack(side=tk.LEFT, padx=5)
+
+initial_list = list(range(1, 21))  # Numbers from 1 to 20 exactly once
+random.shuffle(initial_list)
+
+sorting_algorithms = {
+    'Bubble Sort': bubble_sort,
+    'Insertion Sort': insertion_sort,
+    'Selection Sort': selection_sort,
+    'Merge Sort': merge_sort,
+}
+
+initialize_window(initial_list)
+root.mainloop()
